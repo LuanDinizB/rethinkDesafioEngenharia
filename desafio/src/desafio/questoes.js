345e499324cbd6b443751a8e1468dbd81c0896e4
Luan Diniz <luanminasgerais@hotmail.com>
2022-11-01 01:49:44 -0300

    questoes do desafio resolvidas

---
+++ desafio/src/desafio/questoes.js
@@ -0,0 +1,59 @@
+const pessoas = [
+    {name: "Fabiana Araújo", age: 33 },
+    {name: "Gabriel Gomes", age: 25 },
+    {name: "Fernando Henrique", age: 17 },
+    {name: "Ana Luiza", age: 2 },
+    {name: "Geralda do Nascimento", age: 93 },
+    {name: "Miguel Souza", age: 70 },
+    {name: "Antonio Miguel", age: 69 }
+]
+
+ 
+// -------------------------------------------------------------------
+// 3)
+
+function buscarPeloNome(nome){
+   return pessoas.find((pessoa) => pessoa.name === nome);
+}
+console.log(buscarPeloNome("Gabriel Gomes"));
+
+
+// --------------------------------------------------------------------
+// 4)
+
+
+const nomesRetornados = pessoas.map(function(e){
+    return e.name;
+})
+
+//bonus:
+
+  const nomesRetornadosBonus = pessoas.map(function(e){
+    const split = e.name.split(' ');
+    return split[0];
+  });
+
+// -------------------------------------------------------------------
+// 5)
+
+const inserirId = pessoas.map(function(element, indice){
+    element.id = indice + 1;
+    return element;
+})
+
+// -------------------------------------------------------------------
+// 6)
+
+function filtrar(element) {    
+    return element.age > 18;
+}
+
+const resposta = pessoas.filter(filtrar);
+
+// -------------------------------------------------------------------
+// 7)
+
+const mediaIdades = pessoas.reduce(
+  function(soma, atual){      
+  return (soma + atual.age);
+}, 0)/pessoas.length;
