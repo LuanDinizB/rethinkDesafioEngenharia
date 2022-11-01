d22a702f4721d9c445f940787080a8a47dfcbfdc
Luan Diniz <luanminasgerais@hotmail.com>
2022-11-01 15:09:07 -0300

    formatado com prettier

--- desafio/src/desafio/questoes.js
+++ desafio/src/desafio/questoes.js
@@ -1,60 +1,57 @@
 const pessoas = [
-    {name: "Fabiana Araújo", age: 33 },
-    {name: "Gabriel Gomes", age: 25 },
-    {name: "Fernando Henrique", age: 17 },
-    {name: "Ana Luiza", age: 2 },
-    {name: "Geralda do Nascimento", age: 93 },
-    {name: "Miguel Souza", age: 70 },
-    {name: "Antonio Miguel", age: 69 }
-]
+  { name: "Fabiana Araújo", age: 33 },
+  { name: "Gabriel Gomes", age: 25 },
+  { name: "Fernando Henrique", age: 17 },
+  { name: "Ana Luiza", age: 2 },
+  { name: "Geralda do Nascimento", age: 93 },
+  { name: "Miguel Souza", age: 70 },
+  { name: "Antonio Miguel", age: 69 },
+];
 
- 
 // -------------------------------------------------------------------
 // 3)
 
-function buscarPeloNome(nome){
-   return pessoas.find((pessoa) => pessoa.name === nome);
+function buscarPeloNome(nome) {
+  return pessoas.find((pessoa) => pessoa.name === nome);
 }
 //console.log(buscarPeloNome("Gabriel Gomes"));
-
 
 // --------------------------------------------------------------------
 // 4)
 
-
-const nomesRetornados = pessoas.map(function(e){
-    return e.name;
-})
+const nomesRetornados = pessoas.map(function (e) {
+  return e.name;
+});
 
 //bonus:
 
-  const nomesRetornadosBonus = pessoas.map(function(e){
-    const split = e.name.split(' ');
-    return split[0];
-  });
+const nomesRetornadosBonus = pessoas.map(function (e) {
+  const split = e.name.split(" ");
+  return split[0];
+});
 
 // -------------------------------------------------------------------
 // 5)
 
-const inserirId = pessoas.map(function(element, id){
-    id = id+1;
-    element = {id, ...element}
-    return element;
-})
-console.log(inserirId)
+const inserirId = pessoas.map(function (element, id) {
+  id = id + 1;
+  element = { id, ...element };
+  return element;
+});
+console.log(inserirId);
 
 // -------------------------------------------------------------------
 // 6)
 
-function filtrar(element) {    
-    return element.age > 18;
+function filtrar(element) {
+  return element.age > 18;
 }
 const resposta = pessoas.filter(filtrar);
 
 // -------------------------------------------------------------------
 // 7)
 
-const mediaIdades = pessoas.reduce(
-  function(soma, atual){      
-  return (soma + atual.age);
-}, 0)/pessoas.length;
+const mediaIdades =
+  pessoas.reduce(function (soma, atual) {
+    return soma + atual.age;
+  }, 0) / pessoas.length;
